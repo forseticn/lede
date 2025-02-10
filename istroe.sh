c69bdb9882524b5b69a247f9459be14ca1470f6c
forseti <63159639+forseticn@users.noreply.github.com>
2025-02-10 08:23:18 +0800

    Create istroe.sh

---
+++ istroe.sh
@@ -0,0 +1,16 @@
+# 设置默认IP地址
+sed -i 's/192.168.100.1/10.0.0.3/g' package/istoreos-files/Makefile
+sed -i 's/192.168.1.1/10.0.0.3/g' package/base-files/files/bin/config_generate
+
+
+# 清除登陆密码
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
+
+# 调整 Docker 到 服务 菜单
+# sed -i 's/"admin"/"admin", "services"/g' feeds/luci/applications/luci-app-dockerman/luasrc/controller/*.lua
+# sed -i 's/"admin"/"admin", "services"/g; s/admin\//admin\/services\//g' feeds/luci/applications/luci-app-dockerman/luasrc/model/cbi/dockerman/*.lua
+# sed -i 's/admin\//admin\/services\//g' feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/*.htm
+# sed -i 's|admin\\|admin\\/services\\|g' feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/container.htm
+
+./scripts/feeds update -a
+./scripts/feeds install -a
