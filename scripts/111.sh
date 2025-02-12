b61a4671d206181ab127b2b386d7fc80f6fce673
forseti <63159639+forseticn@users.noreply.github.com>
2025-02-12 14:51:20 +0800

    Create 111.sh

---
+++ scripts/111.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+. /etc/os-release
+. /lib/functions/uci-defaults.sh
+
+# 设置LAN口DNS
+uci set network.lan.dns='127.0.0.1'
+uci commit network
+/etc/init.d/network restart
+
+# 默认wan口防火墙打开
+uci set firewall.@zone[1].input='ACCEPT'
+uci commit firewall
+
+# 设置所有网口可访问网页终端
+uci delete ttyd.@ttyd[0].interface
+
+# 设置所有网口可连接 SSH
+uci set dropbear.@dropbear[0].Interface=''
+uci commit
+
+exit 0
