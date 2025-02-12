16cb6844f6eb24ed0097faaf1e081aa56e2070e8
forseti <63159639+forseticn@users.noreply.github.com>
2025-02-12 14:58:03 +0800

    Update istore.sh

--- istore.sh
+++ istore.sh
@@ -1,7 +1,5 @@
 # 设置默认IP地址
-# sed -i 's/192.168.100.1/10.0.0.3/g' package/istoreos-files/Makefile
-# sed -i 's/192.168.1.1/10.0.0.3/g' package/base-files/files/bin/config_generate
-
+sed -i 's/192.168.100.1/10.0.0.3/g' package/istoreos-files/Makefile
 
 # 清除登陆密码
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
